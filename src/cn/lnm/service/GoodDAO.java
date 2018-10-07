82c9551422cc0a4e2fe7621ca4194540b55307fe
lnmmei <541396412@qq.com>
2018-10-07 23:03:38 +0800

    second commit

---
+++ src/cn/lnm/service/GoodDAO.java
@@ -0,0 +1,11 @@
+package cn.lnm.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoodDAO {
+	public List<String> findAll(){
+		List<String> lt=new ArrayList<>();
+		return lt;
+	}
+}
